26ea64e6be27b91e37146686f8f6dc7208e23b39
hackapest <hackapest@gmail.com>
2024-02-16 08:48:28 -0500

    finished lab2

--- lab2/ShellSort.py
+++ lab2/ShellSort.py
@@ -3,11 +3,14 @@ import time
 
 def generate_random_array(length, min_val=0, max_val=100):
     return [random.randint(min_val, max_val-1) for _ in range(length)]
-
+ 
 def shell_sort(arr):
     n = len(arr)
     gap = 1
     counter = 1
+
+    #m = 0 # copy amount
+    #c = 0 # comparison amount
 
     while gap <= n:
         
@@ -16,27 +19,51 @@ def shell_sort(arr):
             temp = arr[i]
             
             j = i
+
             
             while j >= gap and arr[j - gap] > temp:
                 arr[j] = arr[j - gap]
+                #m += 1
+                #c += 1
                 j -= gap
             
+
             arr[j] = temp
+            #m += 1
 
         counter += 1
         gap = 2 ** counter - 1
 
     return arr
+    #return m, c
+
 
 
 arr = generate_random_array(100)
 
-st = time.time()
-
 shell_sort(arr)
 
+
+# avarage c and m calculation
+"""all_m, all_c = 0, 0
+for i in range(0, 2):
+    arr = generate_random_array(10000)
+    m, c = shell_sort(arr)
+    all_m += m
+    all_c += c
+    
+all_m /= 2
+all_c /= 2
+
+print(f"avarage m - {all_m}\navarabe c - {all_c}")"""
+
+
+
+"""
+st = time.time()
 et = time.time()
 
 elapsed_time = et - st
 print('Execution time:', elapsed_time, 'seconds')
+"""
 
--- lab2/bubbleSort.py
+++ lab2/bubbleSort.py
@@ -4,27 +4,52 @@ import time
 def generate_random_array(length, min_val=0, max_val=100):
     return [random.randint(min_val, max_val-1) for _ in range(length)]
 
+
+
 def bubble_sort(arr):
+    #m = 0 # copy amount
+    #c = 0 # comparison amount
     n = len(arr)
     for i in range(n):
         for j in range(0, n-i-1):
-            if arr[j] > arr[j+1]:
+            if arr[j] > arr[j+1]: 
                 arr[j], arr[j+1] = arr[j+1], arr[j]
+                #m += 1
+            #c += 1
+
+    return arr
+    #return m, c
 
 
 
 
-arr = generate_random_array(10)
+arr = generate_random_array(100)
 
-st = time.time()
-
-
-bubble_sort(arr)
 print(arr)
 
+print(bubble_sort(arr))
+
+# avarage c and m calculation
+"""
+all_m, all_c = 0, 0
+for i in range(0, 100):
+    arr = generate_random_array(100)
+    m, c = bubble_sort(arr)
+    all_m += m
+    all_c += c
+    
+all_m /= 100
+all_c /= 100
+
+print(f"avarage m - {all_m}\navarabe c - {all_c}")"""
+
+
+#time calculation
+"""
+arr = generate_random_array(10000)
+st = time.time()
+bubble_sort(arr)
 et = time.time()
 
 elapsed_time = et - st
-print('Execution time:', elapsed_time, 'seconds')
-
-
+print('Execution time:', elapsed_time, 'seconds')"""
